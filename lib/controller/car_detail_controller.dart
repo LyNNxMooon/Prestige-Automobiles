0be8ea0e057d9e76b52f2a76d1051fbd763d59b8
MSI <kyawlinthant128380@gmail.com>
2024-06-12 16:40:08 +0630

    test

---
+++ lib/controller/car_detail_controller.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:prestige_automobile/data/car_vo.dart';
+import 'package:prestige_automobile/firebase/firebase.dart';
+
+class CarDetailController extends GetxController {
+  final _firebaseService = FirebaseServices();
+
+  Future saveCar(
+      String model,
+      String name,
+      String category,
+      int purchasePrice,
+      int sellingPrice,
+      int year,
+      String description,
+      int id,
+      String url) async {
+    final car = CarVO(
+        id: id,
+        url: url,
+        model: model,
+        name: name,
+        category: category,
+        purchasePrice: purchasePrice,
+        sellingPrice: sellingPrice,
+        year: year,
+        description: description);
+    return _firebaseService.saveCar(car);
+  }
+
+  Future deleteCar(int id) => _firebaseService.deleteCar(id);
+}
